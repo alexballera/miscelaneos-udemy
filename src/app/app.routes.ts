0d2b4276bbae3bb0ed81f644f9582963ff3aec3f
Alex Ballera <alexballera@gmail.com>
2019-11-30 16:06:21 -0300

    Implementando rutas hijas en un archivo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
-import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
-import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
-import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { USUARIOS_ROUTES } from './components/usuario/usuario.routes';
 
 
 const APP_ROUTES: Routes = [
@@ -11,20 +9,9 @@ const APP_ROUTES: Routes = [
   {
     path: 'usuario/:id',
     component: UsuarioComponent,
-    children: [
-      { path: 'nuevo', component: UsuarioNuevoComponent },
-      { path: 'editar', component: UsuarioEditarComponent },
-      { path: 'detalle', component: UsuarioDetalleComponent },
-      { path: '**', pathMatch: 'full', redirectTo: 'editar' },
-    ]
+    children: USUARIOS_ROUTES
   },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
-
-  // { path: 'staticPath', component: ... },
-  // { path: 'path/:routeParam', component: MyComponent },
-  // { path: '**', component: ... },
-  // { path: 'oldPath', redirectTo: '/staticPath' },
-  // { path: ..., component: ..., data: { message: 'Custom' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
